9f243931408cf15185a05424bb0d92ee888998db
Bangwahyu14 <whagung553@gmail.com>
2024-06-03 15:11:43 +0700

    membuat penanda 1

--- exeptionStandar/exeptionStandar.cpp
+++ exeptionStandar/exeptionStandar.cpp
@@ -4,3 +4,11 @@
 #include <array>
 //untuk objek array yang di gunakan
 using namespace std;
+
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 8,4,2 };
+	}
+}
