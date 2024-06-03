29df7aa7ff3ce7a04a94b2a91915cecfec9e066f
Bangwahyu14 <whagung553@gmail.com>
2024-06-03 15:02:44 +0700

    selesai membuat try

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main()
 {
 	try {
-		
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//trow 0.5; //melemparkan sebuah interger maka
+		cout << "pernyataan tidak akan dieksekusi" << endl;
 	}
 }
